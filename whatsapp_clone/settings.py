d1e8423019f9e865aa828c89df00858c4c823103
nareshb07 <k.nareshb0077@gmail.com>
2024-03-27 15:42:08 +0530

    first commit

--- whatsapp_clone/settings.py
+++ whatsapp_clone/settings.py
@@ -170,14 +170,14 @@ ASGI_APPLICATION = 'whatsapp_clone.asgi.application'
 
 
 
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1:8000', 6379)],
-        },
-    },
-}
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
+#         'CONFIG': {
+#             "hosts": [('127.0.0.1:8000', 6379)],
+#         },
+#     },
+# }
 
 # CHANNEL_LAYERS = {
 #     'default': {
